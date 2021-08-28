300464b16cfdf18d0e1b7d9ad9b3335616e096c8
cj05 <cjcoolsuper@gmail.com>
2021-08-28 23:04:06 +0700

    Update NoExit.bat

--- Locker/bin/SecretFolder/NoExit.bat
+++ Locker/bin/SecretFolder/NoExit.bat
@@ -1,6 +1,7 @@
 cls
 @echo off
 Echo this runned in the secret folder, and you never can find it
-cd C:\Users\Admin\Desktop\Locker\
+cd /d %UserProfile%
+cd Desktop\Locker\
 if not exist Welcome.txt call locker.bat
 pause
