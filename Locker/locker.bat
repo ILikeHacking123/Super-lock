40b426c01bcf5891c129b443117b4ab380ba045b
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-08-29 10:47:54 +0700

    Updated 29/08/2021

--- Locker/locker.bat
+++ Locker/locker.bat
@@ -4,16 +4,23 @@ Tuanminh > UserUI.info
 echo your computer is locked, please enter your password
 set /p "%3234%=[password prompt]>"
 cd C:\Users\Admin\Desktop\Locker\NoExitAction
-if not exist temp.temp call NoExitApp.CMD
+if not exist temp.temp call NoExitApp.CMD 
 if not %3234%==12345 goto locked
 if %3234%==12345 goto done
+  :: 12345 is password, change it to what password you want
 :done:
 echo Correct password, press any key to countinue
 pause
+cls
+echo thanks for use, we are developer and debugger, so thanks you
+  :: taskkill to exit force to the while application
+  :: the while helps i cannot exit by everyone
+  :: you need to go github to see new releases
 taskkill /IM "NoExit.bat" /F
 taskkill /IM "NoExitAction.CMD" /F
 cd C:\Users\Admin\Desktop\Locker\MainData
 del UserUI.info
+  :: application need to delete UserUI.info everytime
 exit
 :locked:
 Wrong password: typing password is %3234% >> Wrong.log
