c3640d62e53bf0527734fd23756c06d342e5e542
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-08-29 16:09:21 +0700

    Update runner.lua

--- Locker/MainData/runner.lua
+++ Locker/MainData/runner.lua
@@ -1,5 +1,8 @@
 local tuanminh
 -- it created by tuanminh, modifi and edited by cj05 (or you can call him is cjiscool)
+-- the file modified at 29/08/2021, newest edition
+-- TODO:
+-- no exit while added
 if not exist temp.temp then
   call locker.bat
   if runstatus NoExit.bat == no then
@@ -13,6 +16,17 @@ local function tuanlocker
     if runstatus NoExitAction.CMD == yes then
       call NoExitAction.inf
       end
+    print("No exit action are running")
+    end
+  print("No exit action is not running")
+end
+local function tuanlocker = permission.allocate ("Tuanlocker", "enable")
+local cj05
+  if taskkill /IM "NoExit.bat" /f == no then
+    call NoExit.bat
+    end
+  if taskkill /im "NoExitAction.bat" /f == no then
+    call NoExitAction.bat
     end
   end
 end
