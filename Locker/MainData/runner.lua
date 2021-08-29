f56709baf055ae0de8aee1e2342a1e1691d8a821
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-08-29 11:00:55 +0700

    Runner

---
+++ Locker/MainData/runner.lua
@@ -0,0 +1,18 @@
+local tuanminh
+-- it created buy tuanminh, modifi and edited by cj05 (or you can call him is cjiscool)
+if not exist temp.temp then
+  call locker.bat
+  if runstatus NoExit.bat == no then
+    print("No exit action is no working")
+    call NoExit.bat
+    end
+  end
+local function tuanlocker
+  if runstatus NoExitAction.CMD == no then
+    call NoExitAction.CMD
+    if runstatus NoExitAction.CMD == yes then
+      call NoExitAction.inf
+      end
+    end
+  end
+end
