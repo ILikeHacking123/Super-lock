6723a173081f24bc55666772f43bffcf61412bcb
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-08-30 09:51:39 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,30 @@
+local tuanminh
+if %internet%=%update% do
+    ren.MainCode(121,122)
+    if %update%=1 then
+      https://GitHub.com/TuanminhDev/Tuanminh-project
+      Get.file ()
+      end
+    if else then
+      print("No update")
+      end
+    end
+  end
+local function internet = %internet%
+  %internet% == https://GitHub.com/TuanminhDev/Tuanminh-project
+  if %internet%=https://GitHub.com/TuanminhDev/Tuanminh-project then
+    connect ()
+    if else then
+      disconnect ()
+      end
+    if %internet%= IP.Get then
+      print("oops")
+      end
+    if %internet%= %get% do
+        https://github.com/TuanminhDev/Tuanminh-project/releases
+        connect ()
+        end
+      end
+    end
+  end
+end
